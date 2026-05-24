175206946804aaf6efbc64eeaeea509dc5d320fe
Jaroslav Dobkevich <yaroslav.dobkevich@gmail.com>
2026-05-24 22:19:03 +0300

    refactor(docs): make minor changes to the outputs (instructions)

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "_01_instructions" {
   value = <<-EOT
 
-    STEPS TO BOOTSTRAP A NEW GCP PROJECT
+    A NEW GCP PROJECT INITIALIZATION AND NEXT STEPS
 
     STAGE 1: INITIAL RUN (USING LOCAL STATE)
        Create a terraform.tfvars file and define values as explained in the README.
@@ -26,7 +26,7 @@ output "_01_instructions" {
 output "_02_instructions" {
   description = "Commands to configure GitHub Actions variables using GitHub CLI (gh)"
   value = var.enable_github_wif ? format(
-    "\nSTAGE 4: GITHUB ACTIONS SETUP\n   At this point, the GCP project is ready to be configured with Terraform\n   via CI/CD pipelines using GitHub Actions.\n   Copy the provided cicd_github_actions.yaml into .github/workflows/ in your\n   Terraform repositories and run GitHub CLI (gh) commands in the target\n   repositories (or define these variables directly at github.com):\n\n   gh variable set GCP_PROJECT_ID --body \"%s\"\n   gh variable set GCP_WIF_PROVIDER --body \"%s\"\n   gh variable set GCP_WIF_SERVICE_ACCOUNT --body \"%s\"\n",
+    "\nSTAGE 4: GITHUB ACTIONS SETUP\n   Copy the provided cicd_github_action.yaml into .github/workflows/ in your\n   Terraform repositories and run GitHub CLI (gh) commands in the target\n   repositories (or define these variables directly at github.com):\n\n   gh variable set GCP_PROJECT_ID --body \"%s\"\n   gh variable set GCP_WIF_PROVIDER --body \"%s\"\n   gh variable set GCP_WIF_SERVICE_ACCOUNT --body \"%s\"\n",
     var.project_id,
     module.iam_wif.workload_identity_provider,
     module.iam_wif.admin_service_account_email
@@ -38,16 +38,16 @@ output "_03_instructions" {
 
     IAM AND USER SETUP DETAILS
     
-    1. Human Admin (${var.admin_email}) has 'Editor', 'Project IAM Admin' and 'Billing Project Manager' roles.
+    1. Human Admin (${var.admin_email}) has the 'Editor', 'Project IAM Admin', and 'Billing Project Manager' roles.
 
     2. Standard User (${var.standard_user_email}) has the 'Viewer' role.
 
     3. Service Account '${module.iam_wif.admin_service_account_email}'
-       has 'Editor' and 'Project IAM Admin' roles.
+       has the 'Editor' and 'Project IAM Admin' roles.
 
-    4. To use a Service Account, use impersonation instead of JSON keys (they are less secure and may be disabled).
+    4. To use a service account, use impersonation instead of JSON keys (they are less secure and may be disabled).
 
-    5. To use the Service Account via CLI (after terraform apply):
+    5. To use the service account via CLI (after terraform apply):
        gcloud config set auth/impersonate_service_account ${module.iam_wif.admin_service_account_email}
 
        After this, all your 'gcloud' and 'terraform' commands will run
